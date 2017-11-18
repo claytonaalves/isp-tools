954ff2929b6555273ca7617edfe79902c63044f1
Clayton A. Alves <clayton.aa@gmail.com>
2017-11-18 16:36:25 -0300

    Adicionado limite de data para bloqueio

--- bloqueio.py
+++ bloqueio.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 # coding: utf8
 
+import sys
+import datetime
 from cliente import clientes_com_boletos_vencidos
 
 def main():
+    # O bloqueio só deverá ser efetuado nos seguintes dias:
+    dias_de_bloqueio = [01, 06, 11, 16, 21, 26]
+    hoje = datetime.date.today()
+
+    #if not (hoje.day in dias_de_bloqueio):
+    #    sys.exit(0)
+
     for cliente in clientes_com_boletos_vencidos():
         cliente.bloqueia()
 
